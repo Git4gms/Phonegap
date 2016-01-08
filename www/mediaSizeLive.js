e7f508b49cfc0a9d755dfc465cfb10009f092c2c
Gokul M.S <Gokulms@nl.neologix.in>
2016-01-08 15:36:19 +0530

    toast added commit5

--- www/mediaSizeLive.js
+++ www/mediaSizeLive.js
@@ -5,8 +5,8 @@ var mediaRecLive = {
             errorCallback, // error callback function
             'MediaSizeCut', // mapped to our native Java class called "MediaSizeCut"
             'onClick1', // with this action name
-            [maxSize],
+            [maxSize]
         ); 
     }
-}
+};	
 module.exports = mediaRecLive;
